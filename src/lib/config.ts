c72c3853c46cc21de27cec465c6c69cfbc6bb7ac
NickPetrilli <Nicholas.Petrilli1@marist.edu>
2026-08-29 23:27:44 -0400

    Rename app to Jenna's Closet

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,6 @@
 /**
  * App-wide constants. Rename the app in one place here.
  */
-export const APP_NAME = "Armoire";
+// export const APP_NAME = "Armoire";
+export const APP_NAME = "Jenna's Closet";
 export const APP_TAGLINE = "Personal wardrobe";
